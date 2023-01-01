ca2f01a79580325236a8d49a4447d0a2b6231c52
Nilufa Khan <khannilufa5@gmail.com>
2023-01-01 23:21:52 +0530

    complete

--- src/main/java/com/example/demopc1/controller/CustomerController.java
+++ src/main/java/com/example/demopc1/controller/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customerdata/v1")
+
 public class CustomerController {
 
     CustomerService customerService;
